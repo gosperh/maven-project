3dfe1cff52883a3ef6b63d7f49835a9632344952
COMSUPERgosperh <heya.gosper@csc.gov.au>
2018-10-10 09:29:06 +1100

    added new test for header

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,13 +3,23 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-    @Test
+    private static final String CONTENT_TYPE_JSON = "application/json";
+
+	@Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	MuleEvent event = runFlow("retrieveFlights");
+    	String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    	assertEquals(CONTENT_TYPE_JSON, contentType);
     }
     
     @Override
